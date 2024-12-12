15f5353db7ebdae2069e5532364676ee949cc9e9
mattv8 <matt@visnovsky.us>
2024-12-12 11:41:45 -0700

    Fix composer error

--- multisite-url-fixer.php
+++ multisite-url-fixer.php
@@ -8,7 +8,7 @@
 // Load Bedrock's autoload to ensure environment variables are available
 require_once(dirname(__DIR__, 4) . '/vendor/autoload.php');
 require_once(dirname(__DIR__, 4) . '/config/application.php');
-require_once(__DIR__ . '/src/URLFixer.php');
+require_once(__DIR__ . '/URLFixer.php');
 
 use URLFixer\URLRewrite;
 (new URLRewrite())->addFilters();
